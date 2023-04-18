33e513374d8604d48b36246069acd798189a34f6
Kevin Napier <kjnapier@umich.edu>
2023-04-18 18:05:24 -0400

    changing description

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='heliostack',
     version='0.0.1a',
-    description='A Python Package for Solar System Ephemerides and Dynamics.',
+    description='A Python library for prototyping long-baseline shift-and-stack.',
     author='Kevin J. Napier',
     author_email='kjnapier@umich.edu',
     url="https://github.com/kjnapier/heliostack",
-    install_requires=['torch',
-                      'numpy']
+    install_requires=['torch >= 2.0.0',
+                      'numpy',
+                      'spacerocks >= 2.3.4']
 )
